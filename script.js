8f6e1bd581fc90631d1a580a7fa2ea9e88319cd4
Johannm21 <137768684+Johannm21@users.noreply.github.com>
2023-06-30 10:58:01 +0200

    Version 1.1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+function handleSubmit(){
+  var Vorname=document.getElementById('Vorname').value;
+  var Nachname=document.getElementById('Nachname').value;
+  var Straße=document.getElementById('Straße').value;
+  var Hausnummer=document.getElementById('Hausnummer').value;
+  var Postleitzahl=document.getElementById('Postleitzahl').value;
+  var Ort=document.getElementById('Ort').value;
+  var date1=document.getElementById('date1').value;
+  var date2=document.getElementById('date2').value;
+  
+}
